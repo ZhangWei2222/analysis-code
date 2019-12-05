0396ff6ade025010d8cddf3612d9721e77962829
zhangwei57 <zhangwei57@xiaomi.com>
2019-12-05 11:29:37 +0800

    测试git 重命名

---
+++ src/components/rename.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="rename">
+    我想测试 git重命名 提交
+  </div>
+</template>
+
+<script>
+export default {
+  name: "rename",
+  data() {
+    return {};
+  },
+  mounted() {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
